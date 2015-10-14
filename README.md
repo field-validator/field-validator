3362d7a097f4ddffa49558be5c0dcf90c1d282a0
wangpin <pin.wang@hp.com>
2015-10-14 12:51:56 +0800

    Add grunt,build script

--- README.md
+++ README.md
@@ -7,8 +7,8 @@ flexible field validator
 ### Open sample
 
  ```
-//Define a pattern
-var pattern = {
+//Define patterns
+var patterns = {
   name: {
     required: true
   },
@@ -18,8 +18,8 @@ var pattern = {
   }
 };
 
-//Generate validator by the pattern
-var validator = fv.gen(pattern);
+//Generate validator by the patterns
+var validator = fv.gen(patterns);
 
 //Then perform validation
 var result = validator.perform(data);
@@ -28,6 +28,41 @@ var result = validator.perform(data);
 
 The result should contains standard errors if data is invalid. Or a empty object.
 I suppose fv is the reference of field-validator. Actually, I like short name,don't you?
+
+
+### Additional Usage
+
+* Strict Model
+
+By default the validator is not strict model, it means these passed fields will not make error occurs even if part of them are not defined in patterns.
+
+The strict property could be passed when init validator follwing
+```
+fv.gen(patterns,{ strict: true });
+```
+
+* Check all fields
+
+By default the validation will be breaked when it meet a invalid field.  And there is another model that all fields will be verified no matter any invalid fields.  You can enable this model by following.
+
+```
+fv.gen(patterns,{ all: true });
+```
+
+And it is called allSettled model;
+
+
+### Local development
+
+#### Build
+```
+npm run-script build
+```
+
+#### Unit test
+```
+npm test
+```
 
 ## LICENSE
 
--- src/fieldValidator.js
+++ src/fieldValidator.js
@@ -1,51 +1,100 @@
 (function(w) {
 
-  function validator(pattern){
+  function validator(pattern, options) {
     this.pattern = pattern;
+    this.patternItems = Object.getOwnPropertyNames(pattern);
+    this.strict = false;
+    this.all = false;
+    this.options = options;
+    this.init();
   }
 
-  validator.prototype.perform = function(data){
-    var pattern = this.pattern,
-        errors = [];
+  validator.prototype.init = function() {
+    var v = this,
+      options = v.options;
+    if (options) {
+      v.strict = v.options.strict;
+      v.all = v.options.all;
+    }
+
+  }
+
+  validator.prototype.perform = function(data) {
+    var v = this,
+      pattern = this.pattern,
+      errors = [],
+      dataFields = Object.getOwnPropertyNames(data),
+      patternItems = v.patternItems;
 
     //Passed in data precheck, it should be an object
-    if(!data||(typeof data !== 'object')){
+    if (!data || (typeof data !== 'object')) {
       throw 'Passed in data is null,or undefined,or not an object';
     }
 
-    for(var key in pattern){
+    if (v.strict && (dataFields.length > patternItems.length)) {
+      throw 'Passed in data has fields which are not supported';
+    }
+
+    //Check if corrsponding fields meet the constraint
+    for (var key in pattern) {
       var item = pattern[key];
 
-      if(item.required&&(typeof data[key] === 'undefined')){
-        errors.push([key,' is not provided'].join(''));
-        continue;
+      //Check required constraint
+      if (item.required && (typeof data[key] === 'undefined')) {
+        errors.push([key, ' is not provided'].join(''));
+        if (v.all) {
+          continue;
+        } else {
+          break;
+        }
       }
 
-      if(item.type&&(typeof data[key] !== item.type)){
-        errors.push([key,' is not ',item.type].join(''));
-        continue;
+      //Check type,length,range constraint
+      if (item.type && (typeof data[key] !== item.type)) {
+        errors.push([key, ' is not ', item.type].join(''));
+        if (v.all) {
+          continue;
+        } else {
+          break;
+        }
       }
-
     }
-    return {errors:errors};
+
+    if (v.strict) {
+      //Check if there are fields which are not supported
+      for (var dk in data) {
+        if (!pattern.hasOwnProperty(dk)) {
+          errors.push([dk, ' is not supported']);
+          if (v.all) {
+            continue;
+          } else {
+            break;
+          }
+        }
+      }
+    }
+
+    return {
+      errors: errors
+    };
   }
 
-  validator.prototype.compile = function(newPattern){
+  validator.prototype.compile = function(newPattern) {
     this.pattern = newPattern;
   }
 
   var fv = {
 
     //Generate validator by pattern obj
-    gen:function(pattern){
-      if(!pattern||(typeof pattern !== 'object')){
+    gen: function(pattern, options) {
+      if (!pattern || (typeof pattern !== 'object')) {
         throw 'Passed in pattern should be an object';
       }
-      return new validator(pattern);
+      return new validator(pattern, options);
     }
   };
 
-  if(!w.fieldValidator){
+  if (!w.fieldValidator) {
     w.fieldValidator = fv;
     w.fv = w.fieldValidator;
   }
